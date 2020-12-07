426f2da17a7c9f5e0f32c0cc32e13ec9520e17df
milla-romankova <milla-romankova@gmail.com>
2020-12-07 04:09:09 +0200

    add html, css, js

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # benefit
+
+Stack: HTML, SCSS, JS + Animate.css, slick
+
+JS was used for 1-item slider and burger-menu.
+Animate.css was used in slider and burger-menu.
+Slick was uesd for multiple slider.
+
+[Demo](https://milla-romankova.github.io/benefit)
+
+[Figma](https://www.figma.com/file/D41LmLdI2jZ24xb7UOfJ9XZ7/test_task?node-id=0%3A1)
+
+Task:
+
+Общие требования:
+1. Полное визуальное соответствие макету;
+2. Корректное отображение во всех актуальных браузерах;
+3. Чистота кода, минимальный размер html файла, разбиение scss на блоки;
+4. Адаптивная верстка;
+5. Макет должен быть отзывчивым (наведения, анимированное выезжание меню);
+
+Требования к технологиям:
+1. Html, css, javascript;
+2. Препроцессоры - sass/scss, less;
+3. Сборщики - gulp, webpack;
+4. Использование библиотек - на свое усмотрение;
+
+Краткое описание макета:
+1. Header;
+2. Слайдер с баннерами;
+3. Карточки с товарами;
+4. Контент;
+5. Футер (Кнопка со стрелкой скроллит в начало сайта);
---
+++ src/scripts/main.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const slider = document.querySelectorAll('.slider__item');
+const points = document.querySelectorAll('.slider__point');
+const list = document.querySelector('.slider__points');
+const slideRight = document.querySelector('.slider__arrow--right');
+const slideleft = document.querySelector('.slider__arrow--left');
+
+list.addEventListener('click', event => {
+  if (event.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  for (let i = 0; i < points.length; i++) {
+    if (event.target === points[i]) {
+      slider[i].classList.add('slider__item--block');
+      points[i].classList.add('slider__point--block');
+    } else {
+      slider[i].classList.remove('slider__item--block');
+      points[i].classList.remove('slider__point--block');
+    }
+  }
+});
+
+slideRight.addEventListener('click', function() {
+  for (let i = 0; i < slider.length; i++) {
+    if (slider[i].classList.contains('slider__item--block')
+      && (i < slider.length - 1)) {
+      slider[i + 1].classList
+        .add('slider__item--block', 'animate__slideInRight');
+
+      slider[i].classList
+        .remove('slider__item--block', 'animate__slideInRight');
+
+      points[i + 1].classList.add('slider__point--block');
+      points[i].classList.remove('slider__point--block');
+      break;
+    }
+
+    if (slider[i].classList.contains('slider__item--block')
+    && (i === slider.length - 1)) {
+      slider[0]
+        .add('slider__item--block', 'animate__slideInRight');
+
+      slider[i]
+        .classList.remove('slider__item--block', 'animate__slideInRight');
+      points[0].classList.add('slider__point--block');
+      points[i].classList.remove('slider__point--block');
+    }
+  }
+});
+
+slideleft.addEventListener('click', function() {
+  for (let i = 0; i < slider.length; i++) {
+    if (slider[i].classList.contains('slider__item--block')
+      && (i > 0)) {
+      slider[i - 1].classList
+        .add('slider__item--block', 'animate__slideInLeft');
+
+      slider[i].classList
+        .remove('slider__item--block', 'animate__slideInLeft');
+      points[i - 1].classList.add('slider__point--block');
+      points[i].classList.remove('slider__point--block');
+      break;
+    }
+
+    if (slider[i].classList.contains('slider__item--block')
+    && (i === 0)) {
+      slider[slider.length - 1].classList
+        .add('slider__item--block', 'animate__slideInLeft');
+
+      slider[i].classList.remove('slider__item--block', 'animate__slideInLeft');
+      points[slider.length - 1].classList.add('slider__point--block');
+      points[i].classList.remove('slider__point--block');
+      break;
+    }
+  }
+});
+
+const burger = document.querySelector('.nav__burgers');
+const nav = document.querySelector('.nav__list');
+
+burger.addEventListener('click', event => {
+  burger.children[0].classList.toggle('nav__burger--toggle');
+  burger.children[1].classList.toggle('nav__burger--toggle');
+
+  if (event.target === burger.children[0]) {
+    nav.classList.remove('animate__slideOutLeft');
+
+    nav.classList
+      .add('nav__list--toggle', 'animate__slideInLeft');
+  } else {
+    nav.classList.remove('animate__slideInLeft');
+    nav.classList.add('animate__slideOutLeft');
+  }
+});
+
+/* eslint-disable */
+$(document).ready(function() {
+  $('.popular__cards').slick({
+    arrows: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 1750,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 1250,
+        settings: 'unslick',
+      },
+    ],
+  });
+});
